6801d308c61b9e1fe877f1096201f764d8f0de3f
(Student  C00301768) Eliska Vrzalova <C00301768@setu.ie>
2023-11-17 09:42:34 +0000

    Print choice menu Eliska

--- converter/converter/main.cpp
+++ converter/converter/main.cpp
@@ -1,8 +1,25 @@
 //eliska vrzalova
 //RoBert McGregor
 
+#include <iostream>
+
 int main() 
 {
 	
+
+
 	return 0;
+}
+char printMenu() //written by eliska
+{
+	char choice = ' ';
+	std::cout << "Please enter a upper case letter to convert left to right/ lower case to convert from right to left" << std::endl;
+	std::cout << "[A] Degrees  <--->  Radians    [a]" << std::endl;
+	std::cout << "[B] Centimeters  <--->  Feet & Inches [b]" << std::endl;
+	std::cout << "[C] Litres  <--->  Gallons   [c]" << std::endl;
+	std::cout << "[Q] Exit   [---] Exit   [q]" << std::endl;
+	std::cout << "Enter your choice:";
+	std::cin >> choice;
+	return choice;
+
 }
