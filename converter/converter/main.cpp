eb4a9784a235f6944e588e698db2847675054c0e
(Student  C00302210) Robert Mc Gregor <C00302210@setu.ie>
2023-11-17 09:39:49 +0000

    Added second in command half of code.

--- converter/converter/main.cpp
+++ converter/converter/main.cpp
@@ -1,8 +1,23 @@
 //eliska vrzalova
 //RoBert McGregor
 
+#include <iostream>
+
 int main() 
 {
 	
 	return 0;
+}
+
+char printMenu() // written by second in command
+{
+	char choice = ' '; //user input choice
+	std::cout << "Please enter an upper case letter to convert from left to right\nand lower to convert from right to left\n\n" << std::endl;
+	std::cout << "[D] Mpg <---> L/100km [d]" << std::endl;
+	std::cout << "[E] Kilograms <---> Stone & Pounts [e]" << std:: endl;
+	std::cout << "[F] Celsius <---> Fahrenheit [f]" << std::endl;
+	std::cout << "[Q] Exit [---] Exit [q]" << std::endl;
+	std::cout << "Enter your choice: ";
+	std::cin >> choice;
+	return choice;
 }
